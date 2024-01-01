432f8dcffd27a2c514c52f25b96481bc0599b3d6
cvckeboy <kosov-vova@inbox.ru>
2024-01-01 23:48:53 +0300

    v 0.1.0 backend complete

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -4,6 +4,6 @@ from . import serializers
 from . import models
 # Create your views here.
 
-class TodoViews(viewsets.ModelViewSet):
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = models.Todo.objects.all()
     serializer_class = serializers.TodoSerializer
