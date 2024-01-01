af90246bc32c0bd14fe604b34bcfc654a68a583f
cvckeboy <kosov-vova@inbox.ru>
2024-01-01 22:56:28 +0300

    v 0.0.1 backend

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    body = models.CharField( max_length=300)
+    completed = models.BooleanField( default=False)
+    update = models.DateTimeField( auto_now=True)
+    created = models.DateTimeField( auto_now_add=True)
